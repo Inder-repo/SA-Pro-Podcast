87d6edcf4cf1e9abb5d7eff300e4a9ea60daf329
Inder-repo <pushpinder3784@gmail.com>
2026-02-18 16:30:34 +1100

    Update workshop8.py

--- workshop8.py
+++ workshop8.py
@@ -357,7 +357,7 @@ def render_methodology_overview():
         margin=dict(l=20, r=20, t=20, b=20),
         plot_bgcolor='rgba(0,0,0,0)',
         paper_bgcolor='rgba(0,0,0,0)'
-    ))
+    )
     
     st.plotly_chart(fig, use_container_width=True)
 
